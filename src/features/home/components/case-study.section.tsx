8dc217b734c3bd0b8c12d1a0706e0247fa595034
maoudin <mathis.oudin42@gmail.com>
2026-01-14 14:57:26 +0100

    test: test lint

--- src/features/home/components/case-study.section.tsx
+++ src/features/home/components/case-study.section.tsx
@@ -1,21 +1,15 @@
-import React from "react";
+import React from 'react'
 
 type CaseProps = {
-    title: string;
-    description: string;
-    icon: React.JSX.Element;
+  title: string
+  description: string
+  icon: React.JSX.Element
 }
 
-const Case: React.FC = (): React.JSX.Element => {
-    return (
-        <div>sfgdfg</div>
-    )
+const Case: React.FC<CaseProps> = ({ title, description, icon }: CaseProps): React.JSX.Element => {
+  return <div>sfgdfg</div>
 }
 
 export const CaseStudySection: React.FC = (): React.JSX.Element => {
-    return (
-        <section>
-            case study section
-        </section>
-    )
+  return <section>case study section</section>
 }
