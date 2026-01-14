8d5eea0b7d0b287b4d7fbbabe829778c5e9da495
maoudin <mathis.oudin42@gmail.com>
2026-01-14 14:35:22 +0100

    feat: basic case study section

---
+++ src/features/home/components/case-study.section.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type CaseProps = {
+    title: string;
+    description: string;
+    icon: React.JSX.Element;
+}
+
+const Case: React.FC = (): React.JSX.Element => {
+    return (
+        <div>sfgdfg</div>
+    )
+}
+
+export const CaseStudySection: React.FC = (): React.JSX.Element => {
+    return (
+        <section>
+            case study section
+        </section>
+    )
+}
