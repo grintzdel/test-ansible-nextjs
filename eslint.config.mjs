374496941611ae63a7d1a7eb5bc37179c598b6d3
maoudin <mathis.oudin42@gmail.com>
2026-01-14 14:43:40 +0100

    feat: add eslint and prettier config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,7 +15,44 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "*.js",
+    "node_modules/**",
+    "*.json",
+    "*.md",
+    "*.css",
   ]),
+  {
+    plugins: {
+      "only-warn": onlyWarn,
+    },
+    languageOptions: {
+      globals: {
+        React: true,
+        JSX: true,
+        node: true,
+      },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+    },
+  },
+  {
+    files: ["*.js?(x)", "*.ts?(x)"],
+    rules: {
+      "react/jsx-no-literals": "error",
+      "no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "^I[A-Z].*(Interface|Gateway)$",
+        },
+      ],
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
