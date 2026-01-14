6114ff33bd68e51bb18cc22bb84a5632a1d17626
maoudin <mathis.oudin42@gmail.com>
2026-01-14 16:51:08 +0100

    feat: update layout bg and home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
-        <main>{children}</main>
+        <main className="bg-white">{children}</main>
         <Footer />
       </body>
     </html>
