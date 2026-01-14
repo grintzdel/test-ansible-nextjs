6efbca7837b7f796c697233f599bc700d7bc418c
maoudin <mathis.oudin42@gmail.com>
2026-01-14 11:47:04 +0100

    chore: setup app

---
+++ src/features/home/home.page.tsx
@@ -0,0 +1,10 @@
+import {HeroSection} from "@/features/home/components/hero.section";
+import React from "react";
+
+export default function HomePage() {
+  return (
+  <React.Fragment>
+      <HeroSection/>
+  </React.Fragment>
+)
+}
